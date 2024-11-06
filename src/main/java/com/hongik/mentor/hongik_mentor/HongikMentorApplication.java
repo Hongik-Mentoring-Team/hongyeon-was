74bbc33712de2f6cd9e54b4e820a9d8d354ec9d8
WithFortuna <rmsghchl0@naver.com>
2024-11-06 17:18:34 +0900

    setup environment

---
+++ src/main/java/com/hongik/mentor/hongik_mentor/HongikMentorApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.mentor.hongik_mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikMentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongik/mentor/hongik_mentor/HongikMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.mentor.hongik_mentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongikMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
