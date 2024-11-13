d065d44cc7d9a3da3307d3a130094d7ed5fc281a
user <dddsa1313@gmail.com>
2024-11-13 15:32:39 +0900

    lee commit test

--- src/main/java/com/hongik/mentor/hongik_mentor/HongikMentorApplication.java
+++ src/main/java/com/hongik/mentor/hongik_mentor/HongikMentorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HongikMentorApplication {
 
 	public static void main(String[] args) {
+		system.out.println("lee test");
 		SpringApplication.run(HongikMentorApplication.class, args);
 	}
 
