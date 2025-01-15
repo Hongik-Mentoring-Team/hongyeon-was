1d03f45da8201ffa0879770cade220f24d754abe
WithFortuna <rmsghchl0@naver.com>
2025-01-15 17:20:51 +0900

    재학생 인증 기능 추가(테스트 완료) 1. MemberService에 재학생 인증 로직 추가 *참고 사용한 api라이브러리: UnivCert

--- src/test/java/com/hongik/mentor/hongik_mentor/service/MemberServiceTest.java
+++ src/test/java/com/hongik/mentor/hongik_mentor/service/MemberServiceTest.java
@@ -2,13 +2,27 @@ package com.hongik.mentor.hongik_mentor.service;
 
 import com.hongik.mentor.hongik_mentor.domain.Badge;
 import com.hongik.mentor.hongik_mentor.repository.BadgeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 @SpringBootTest
 class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
 
+    @Test
+    void 인증메일_전송_테스트() {
+        boolean result = memberService.requestUnivEmailVerification("c011204@mail.hongik.ac.kr");
+        log.info("결과: {}", result);
+    }
+
+    @Test
+    void 인증코드_확인_테스트() {
+        boolean result = memberService.confirmUnivEmailVerification("c011204@mail.hongik.ac.kr", 2164);
+        log.info("인증코드 확인 테스트 결과: {}", result);
+    }
 }
